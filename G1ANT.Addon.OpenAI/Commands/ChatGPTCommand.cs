919a44a93b462124d6e6b00aae2e7f819ff1652a
KacperBorowiecki <74897687+KacperBorowiecki@users.noreply.github.com>
2023-04-26 15:47:04 +0200

    fix

--- G1ANT.Addon.OpenAI/Commands/ChatGPTCommand.cs
+++ G1ANT.Addon.OpenAI/Commands/ChatGPTCommand.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
 using G1ANT.Language;
 using Newtonsoft.Json;
 using System.Net.Http.Formatting;
