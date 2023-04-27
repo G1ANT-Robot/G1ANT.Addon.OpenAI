ec3a594cd76cafa2d6875a7a7d4a1c63ce924d09
KacperBorowiecki <74897687+KacperBorowiecki@users.noreply.github.com>
2023-04-27 09:29:34 +0200

    removed redundancy

--- G1ANT.Addon.OpenAI/Commands/ChatGPTCommand.cs
+++ G1ANT.Addon.OpenAI/Commands/ChatGPTCommand.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Web;
 using G1ANT.Language;
 using Newtonsoft.Json;
-using System.Net.Http.Formatting;
 using Newtonsoft.Json.Linq;
 
 namespace G1ANT.Addon.OpenAI.Commands
@@ -19,14 +19,14 @@ namespace G1ANT.Addon.OpenAI.Commands
 
             [Argument(Tooltip = "Prompt for ChatGPT", Required = true)]
             public TextStructure Prompt { get; set; }
-
+            // Information about range is from api documentation, but it works also with values outside the range
             [Argument(Tooltip = "Default 1. Range between 0 - 2.  will make the output more random, lower values will make it more focused and deterministic.", Required = true)]
             public IntegerStructure Temperature { get; set; } = new IntegerStructure(1);
 
             [Argument(Tooltip = "If set to true, full JSON response will be returned. If false, only message content will be returned")]
             public BooleanStructure ReturnJson { get; set; } = new BooleanStructure(false);
 
-            [Argument(Tooltip = "Name of a variable where the provided text will be stored")]
+            [Argument(Tooltip = "Name of a variable where the result will be stored")]
             public VariableStructure Result { get; set; } = new VariableStructure("result");
         }
 
@@ -42,18 +42,24 @@ namespace G1ANT.Addon.OpenAI.Commands
             var temperature = arguments.Temperature.Value;
 
             var chatResponse = ChatGPT(apiKey, prompt, model, temperature);
-            var jsonChatResponse = JObject.Parse(chatResponse);
-            JToken messageToken = jsonChatResponse["choices"][0]["message"]["content"];
-            string messageJson = messageToken.ToString();
+            string result = chatResponse.Content.ReadAsStringAsync().Result;
+            var formattedJson = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(result), Formatting.Indented);
+            var jsonChatResponse = JObject.Parse(formattedJson);
 
-            if (arguments.ReturnJson.Value)
-                Scripter.Variables.SetVariableValue(arguments.Result.Value, new JsonStructure(jsonChatResponse));
+            if (chatResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                if (arguments.ReturnJson.Value)
+                    Scripter.Variables.SetVariableValue(arguments.Result.Value, new JsonStructure(jsonChatResponse));
+                else
+                    Scripter.Variables.SetVariableValue(arguments.Result.Value, new TextStructure(jsonChatResponse["choices"][0]["message"]["content"].ToString()));
+            }
             else
-                Scripter.Variables.SetVariableValue(arguments.Result.Value, new TextStructure(messageJson));
-
+            {
+                throw new HttpException((int)chatResponse.StatusCode, jsonChatResponse["error"].ToString());
+            }
         }
 
-        static string ChatGPT(string apiKey, string content, string model, int temperature = 1)
+        static HttpResponseMessage ChatGPT(string apiKey, string content, string model, int temperature = 1)
         {
             var messages = new List<GptMessage>
             {
@@ -66,21 +72,14 @@ namespace G1ANT.Addon.OpenAI.Commands
                 Messages = messages
             };
 
-            string formattedJson = "";
-
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://api.openai.com/");
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
-
                 HttpResponseMessage response = client.PostAsJsonAsync("v1/chat/completions", jsonModel).Result;
-
-                string result = response.Content.ReadAsStringAsync().Result;
-                formattedJson = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(result), Formatting.Indented);
+                return response;
             }
-
-            return formattedJson;
         }
 
         public class GptMessage
